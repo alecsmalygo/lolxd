7941dfeef34b1ea79437429695dc7fdca6792897
a.m.student <ninjaalec12@gmail.com>
2021-01-20 10:04:37 -0600

    Initial Commit

---
+++ lolxd/lolxdApp.swift
@@ -0,0 +1,17 @@
+//
+//  lolxdApp.swift
+//  lolxd
+//
+//  Created by A.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct lolxdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
